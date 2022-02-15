d94256663f0804d10e393248b4b6bb7b4f9686fa
Andrey Mateus <yerdnasuetam@hotmail.com>
2022-02-15 02:47:16 -0300

    Construtores

--- src/sub_classes/Ave.java
+++ src/sub_classes/Ave.java
@@ -4,6 +4,33 @@ import super_classes.Animal;
 public class Ave extends Animal {
     
 
+// Construtores passando parâmetros para os construtores de sua super classe.
+
+public Ave() {
+    super();
+}
+
+public Ave(String nome) {
+    super(nome);
+}
+
+public Ave(String nome, int idade) {
+    super(nome, idade);
+}
+
+public Ave(String raca, char genero) {
+    super(raca, genero);
+}
+
+public Ave(String nome, String raca, int idade, String genero) {
+    super(nome, raca, idade, genero);
+}
+
+public Ave(String nome, int idade, String raca, String genero) {
+    this(nome, idade);
+    setRaca(raca);
+    setGenero(genero);
+}
 
 
 
--- src/sub_classes/Gato.java
+++ src/sub_classes/Gato.java
@@ -3,18 +3,33 @@ package sub_classes;
 import super_classes.Animal;
 
 public class Gato extends Animal {
-    
-    public Gato(){
 
+    // Construtores passando parâmetros para os construtores de sua super classe.
+
+    public Gato() {
+        super();
     }
 
-    public Gato(String nome){
+    public Gato(String nome) {
         super(nome);
     }
 
+    public Gato(String nome, int idade) {
+        super(nome, idade);
+    }
 
+    public Gato(String raca, char genero) {
+        super(raca, genero);
+    }
 
+    public Gato(String nome, String raca, int idade, String genero) {
+        super(nome, raca, idade, genero);
+    }
 
-
+    public Gato(String nome, int idade, String raca, String genero) {
+        this(nome, idade);
+        setRaca(raca);
+        setGenero(genero);
+    }
 
 }
